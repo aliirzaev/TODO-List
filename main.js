55358148b7440d36f3212861d9a8da1586969faa
aliirzaev <you@example.com>
2024-02-13 14:28:01 +0300

    rechanged

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+let add_task_button = document.querySelector(".add_task_button")
 
+class TaskTemplate {
+    constructor() {
+        
+    }
+}
+
+add_task_button.addEventListener("clicl", () => {
+    console.log("only class")
+})
