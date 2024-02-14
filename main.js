764f6efa357d18034dc9c1cb8a85d58c6d89b6a6
aliirzaev <you@example.com>
2024-02-14 20:07:34 +0300

    added

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 let add_task_button = document.querySelector(".add_task_button")
+let task_id = document.querySelector(".task_id")
+let task_name = document.querySelector(".task_name")
+let task_status = document.querySelector(".task_status")
 
-class TaskTemplate {
-    constructor() {
-        
-    }
-}
-
-add_task_button.addEventListener("clicl", () => {
-    console.log("only class")
-})
+let NewTask_frame = document.querySelector(".NewTask")
+let apply_button = document.querySelector(".apply_button")
+let name_inp = document.querySelector(".name_inp")
