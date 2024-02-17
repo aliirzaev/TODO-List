023980fa0fc159b64ecace29da0f27d4b3220ee7
aliirzaev <you@example.com>
2024-02-17 23:17:22 +0300

    new

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 let add_task_button = document.querySelector(".add_task_button")
-let task_id = document.querySelector(".task_id")
-let task_name = document.querySelector(".task_name")
-let task_status = document.querySelector(".task_status")
+
+let list = document.querySelector(".list")
+let template_list = document.querySelector(".template")
+// let template_id = document.querySelector(".template_id")
+// let template_name = document.querySelector(".template_name")
+// let template_status = document.querySelector(".template_status")
 
 let newTask_frame = document.querySelector(".NewTask")
 let apply_button = document.querySelector(".apply_button")
