46a2287864889cf79f20644942ad817d4a635596
aliirzaev <you@example.com>
2024-02-18 20:57:38 +0300

    almost done everything

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-let add_task_button = document.querySelector(".add_task_button")
-
 let list = document.querySelector(".list")
 let template_list = document.querySelector(".template")
-// let template_id = document.querySelector(".template_id")
-// let template_name = document.querySelector(".template_name")
-// let template_status = document.querySelector(".template_status")
 
-let newTask_frame = document.querySelector(".NewTask")
-let apply_button = document.querySelector(".apply_button")
-let name_input = document.querySelector(".name_inp")
+let task_add = document.querySelector(".task_add")
+let name_input = document.querySelector(".name_input")
+let add_task_button = document.querySelector(".add_task_button")
+
+function createTask(name) {
+    let newTemp = template_list.cloneNode(true)
+    newTemp.querySelector(".template_name").innerHTML = name
+    newTemp.querySelector(".template_status").innerHTML = "Ongoing"
+    newTemp.querySelector(".template_id").innerHTML = "#" + (list.childNodes.length + 1)
+    
+    list.append(newTemp)
+    newTemp.style.display = "flex"
+
+    let remove_button = newTemp.querySelector(".template_button_remove")
+    remove_button.addEventListener("click", () => {
+        newTemp.remove(true)
+    })
+}
+
+add_task_button.addEventListener("click", () => {
+    if (name_input.value != "") {
+        createTask(name_input.value)
+        name_input.value = ""
+    }
+})
