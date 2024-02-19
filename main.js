cb2ec4222211edc738adc52971ba9403423da6cc
aliirzaev <you@example.com>
2024-02-19 16:03:27 +0300

    done

--- main.js
+++ main.js
@@ -8,16 +8,28 @@ let add_task_button = document.querySelector(".add_task_button")
 function createTask(name) {
     let newTemp = template_list.cloneNode(true)
     newTemp.querySelector(".template_name").innerHTML = name
-    newTemp.querySelector(".template_status").innerHTML = "Ongoing"
+    let status = newTemp.querySelector(".template_status")
+    status.innerHTML = "In order"
     newTemp.querySelector(".template_id").innerHTML = "#" + (list.childNodes.length + 1)
     
     list.append(newTemp)
     newTemp.style.display = "flex"
 
     let remove_button = newTemp.querySelector(".template_button_remove")
+    let edit_button = newTemp.querySelector(".template_button_edit")
 
     remove_button.addEventListener("click", () => {
         newTemp.remove(true)
+    })
+
+    edit_button.addEventListener("click", () => {
+        if (status.innerHTML == "In order") {
+            status.innerHTML = "Ongoing"
+        } else if (status.innerHTML == "Ongoing") {
+            status.innerHTML = "Completed"
+        } else if (status.innerHTML == "Completed") {
+            status.innerHTML = "In order"
+        }
     })
 }
 
